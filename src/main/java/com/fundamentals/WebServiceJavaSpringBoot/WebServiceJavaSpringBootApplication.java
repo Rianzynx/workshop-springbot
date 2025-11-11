9dffb0c1c94a6e54050deb7cff566b6f9558ac90
Rianzynx <rianeduardodudu@hotmail.com>
2025-11-11 14:34:37 -0300

    Project created

---
+++ src/main/java/com/fundamentals/WebServiceJavaSpringBoot/WebServiceJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentals.WebServiceJavaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentals/WebServiceJavaSpringBoot/WebServiceJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentals.WebServiceJavaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
