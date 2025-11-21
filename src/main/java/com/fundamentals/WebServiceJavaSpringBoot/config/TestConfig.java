e23576f00bf4097f2272b83bfaec4b61f39bfa14
Rianzynx <rianeduardodudu@hotmail.com>
2025-11-21 17:35:30 -0300

    Payment, one-to-one association

--- src/main/java/com/fundamentals/WebServiceJavaSpringBoot/config/TestConfig.java
+++ src/main/java/com/fundamentals/WebServiceJavaSpringBoot/config/TestConfig.java
@@ -89,8 +89,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3,p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2025-11-11T18:21:09Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
-
-
-
 }
