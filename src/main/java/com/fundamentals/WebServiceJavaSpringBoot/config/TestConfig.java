c2f706580107c69f5abff739a2956a53a48f2d13
Rianzynx <rianeduardodudu@hotmail.com>
2025-11-21 15:45:44 -0300

    Order Status Enum

--- src/main/java/com/fundamentals/WebServiceJavaSpringBoot/config/TestConfig.java
+++ src/main/java/com/fundamentals/WebServiceJavaSpringBoot/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.fundamentals.WebServiceJavaSpringBoot.config;
 
 import com.fundamentals.WebServiceJavaSpringBoot.entities.Order;
 import com.fundamentals.WebServiceJavaSpringBoot.entities.User;
+import com.fundamentals.WebServiceJavaSpringBoot.entities.enums.OrderStatus;
 import com.fundamentals.WebServiceJavaSpringBoot.repositories.OrderRepository;
 import com.fundamentals.WebServiceJavaSpringBoot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria", "maria@gmail.com", "14998888888", "1234");
         User u2 = new User(null, "Joao", "joao@gmail.com", "14997777777", "1234");
 
-        Order o1 = new Order(null, Instant.parse("2025-11-11T16:21:09Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2025-12-23T18:29:01Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2025-05-16T16:23:09Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2025-11-11T16:21:09Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2025-12-23T18:29:01Z"), OrderStatus.WAITING_PAYMENTE,u2);
+        Order o3 = new Order(null, Instant.parse("2025-05-16T16:23:09Z"), OrderStatus.WAITING_PAYMENTE, u1);
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2, o3));
